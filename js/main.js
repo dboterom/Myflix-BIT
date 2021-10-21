708ceba0f90a77614c5d53ee21582ca707a54e90
Juan Camilo Garcia <camilojuang11@gmail.com>
2021-10-20 21:38:24 -0500

    funcionalidad add newmovie aparece popup y boton cancelar cierra el popup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var botonAddNew=document.getElementById("addnew");
+var popup=document.getElementById("addnewmovie");
+
+function mostrar(){
+    var display=popup.style.display='block';
+}
+function cerrar(){
+    var cerrarpopup=popup.style.display='none';
+}
